b392a1d9d003718c61f5810b1f5162ffd5be037e
Daniel Świątek <daniel@swiatek.waw.pl>
2025-04-28 09:21:05 +0200

    Stylizacja aplikacji

--- BookWeb/Services/GoogleBooksService.cs
+++ BookWeb/Services/GoogleBooksService.cs
@@ -19,7 +19,9 @@ public class GoogleBooksService
         var response = await _httpClient.GetAsync(url);
 
         if (!response.IsSuccessStatusCode)
+        {
             return new List<Book>();
+        }
 
         using var stream = await response.Content.ReadAsStreamAsync();
         using var doc = await JsonDocument.ParseAsync(stream);
@@ -49,43 +51,40 @@ public class GoogleBooksService
             }
         }
 
-        
-
         return books;
     }
 
     public async Task<Book?> GetBookByIdAsync(string id)
-{
-    var url = $"https://www.googleapis.com/books/v1/volumes/{Uri.EscapeDataString(id)}";
-    var response = await _httpClient.GetAsync(url);
-
-    if (!response.IsSuccessStatusCode)
-        return null;
-
-    using var stream = await response.Content.ReadAsStreamAsync();
-    using var doc = await JsonDocument.ParseAsync(stream);
-
-    var item = doc.RootElement;
-
-    if (item.ValueKind == JsonValueKind.Undefined || item.ValueKind == JsonValueKind.Null)
-        return null;
-
-    var volumeInfo = item.GetProperty("volumeInfo");
-
-    return new Book
     {
-        Id = id,
-        Title = volumeInfo.GetProperty("title").GetString() ?? "Brak tytułu",
-        Authors = volumeInfo.TryGetProperty("authors", out var authors)
-            ? string.Join(", ", authors.EnumerateArray().Select(a => a.GetString()))
-            : "Nieznani autorzy",
-        Description = volumeInfo.TryGetProperty("description", out var desc)
-            ? desc.GetString()
-            : "",
-        Thumbnail = volumeInfo.TryGetProperty("imageLinks", out var img) && img.TryGetProperty("thumbnail", out var thumb)
-            ? thumb.GetString()
-            : ""
-    };
-}
+        var url = $"https://www.googleapis.com/books/v1/volumes/{Uri.EscapeDataString(id)}";
+        var response = await _httpClient.GetAsync(url);
 
+        if (!response.IsSuccessStatusCode)
+            return null;
+
+        using var stream = await response.Content.ReadAsStreamAsync();
+        using var doc = await JsonDocument.ParseAsync(stream);
+
+        var item = doc.RootElement;
+
+        if (item.ValueKind == JsonValueKind.Undefined || item.ValueKind == JsonValueKind.Null)
+            return null;
+
+        var volumeInfo = item.GetProperty("volumeInfo");
+
+        return new Book
+        {
+            Id = id,
+            Title = volumeInfo.GetProperty("title").GetString() ?? "Brak tytułu",
+            Authors = volumeInfo.TryGetProperty("authors", out var authors)
+                ? string.Join(", ", authors.EnumerateArray().Select(a => a.GetString()))
+                : "Nieznani autorzy",
+            Description = volumeInfo.TryGetProperty("description", out var desc)
+                ? desc.GetString()
+                : "",
+            Thumbnail = volumeInfo.TryGetProperty("imageLinks", out var img) && img.TryGetProperty("thumbnail", out var thumb)
+                ? thumb.GetString()
+                : ""
+        };
+    }
 }
--- BookWeb/obj/Debug/net8.0/BookWeb.AssemblyInfo.cs
+++ BookWeb/obj/Debug/net8.0/BookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f344a892945609552e3edf5e8d21ea0f5b62488")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cd6dc7820e2814b339b5f3225c3810e06bd10e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
